508c7d6499760339edea1305197cdf5542766382
matthewweir9 <43483048+matthewweir9@users.noreply.github.com>
2020-01-28 11:07:45 -0700

    Update Library_drawPacman.js

--- Library_drawPacman.js
+++ Library_drawPacman.js
@@ -1,9 +1,1 @@
-function drawPacman (x, y, radius, mouth) {
-  context.beginPath();
-  context.arc(x, y, radius, 0.2 * Math.PI * mouth, 1.8 * Math.PI * mouth);
-  context. lineTo(x, y);
-  context.fillStyle = "yellow";
-  context.fill();
-  context.closePath();
-  context.stroke();
-}
+

b94e4a6245bf9d7765528b609d64b70be2e2483e
matthewweir9 <43483048+matthewweir9@users.noreply.github.com>
2020-01-28 11:14:01 -0700

    Update Library_Asteroids.js

--- Library_Asteroids.js
+++ Library_Asteroids.js
@@ -32,12 +32,12 @@ ctx.restore();
 
 
 //draw pacman function
-function drawPacman (x, y, radius, mouth) {
-  context.beginPath();
-  context.arc(x, y, radius, (0.2 * Math.PI) * mouth, (1.8 * Math.PI) * mouth);
-  context. lineTo(x, y);
-  context.fillStyle = "yellow";
-  context.fill();
-  context.closePath();
-  context.stroke();
-}
+//function drawPacman (x, y, radius, mouth) {
+ // context.beginPath();
+  //context.arc(x, y, radius, (0.2 * Math.PI) * mouth, (1.8 * Math.PI) * mouth);
+ // context. lineTo(x, y);
+ // context.fillStyle = "yellow";
+ // context.fill();
+ // context.closePath();
+ // context.stroke();
+//}

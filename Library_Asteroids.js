bf036f0ad189810cf37bac0d4de8b8c67775511a
matthewweir9 <43483048+matthewweir9@users.noreply.github.com>
2020-02-03 19:36:53 -0700

    Update Library_Asteroids.js

--- Library_Asteroids.js
+++ Library_Asteroids.js
@@ -44,34 +44,52 @@ ctx.stroke();
 
 
 // draw ship
+
 function drawShip(ctx, radius, options) {
 options = options || {};
+let angle = (options.angle || 0.5 * Math.PI / 2);
+// this is new
+let curve = options.curve || 0.5;
 ctx.save();
 if(options.guide) {
 ctx.strokeStyle = "white";
 ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+
 ctx.lineWidth = 0.5;
 ctx.beginPath();
 ctx.arc(0, 0, radius, 0, 2 * Math.PI);
 ctx.stroke();
 ctx.fill();
 }
-ctx.lineWidth = options.lineWidth || 2;
+ctx.lineWidth = options. lineWidth || 2;
 ctx.strokeStyle = options.stroke || "white";
-ctx.fillStyle = options.fill || "black";
-let angle = (options.angle || 0.5 * Math.PI / 2);
+ctx.fillStyle = options. fill || "black";
 ctx.beginPath();
 ctx.moveTo(radius, 0);
 ctx.lineTo(
 Math.cos(Math.PI - angle) * radius,
 Math.sin(Math.PI - angle) * radius
 );
-ctx.lineTo(
+// here we have added a control point based on the curve variable
+ctx.quadraticCurveTo(radius * curve - radius, 0,
 Math.cos(Math.PI + angle) * radius,
 Math.sin(Math.PI + angle) * radius
 );
 ctx.closePath();
 ctx.fill();
 ctx.stroke();
+// a new guide line and circle show the control point
+if(options.guide) {
+ctx.strokeStyle = "white";
+ctx.lineWidth = 0.5;
+ctx.beginPath();
+ctx.moveTo(-radius, 0);
+ctx.lineTo(0, 0);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(radius * curve - radius, 0, radius/50, 0, 2 * Math.PI);
+
+ctx.stroke();
+}
 ctx.restore();
 }

19de1d0f0eb3c0c0f8c8c80f5c1683f308eaee02
matthewweir9 <43483048+matthewweir9@users.noreply.github.com>
2020-02-03 20:01:32 -0700

    Update Library_Asteroids.js

--- Library_Asteroids.js
+++ Library_Asteroids.js
@@ -1,4 +1,4 @@
-//draw grid function
+//draw grid function##################################################################################################################
 function drawGrid(ctx, minor, major, stroke, fill) {
 minor = minor || 10;
 major = major || minor * 5;
@@ -31,7 +31,7 @@ ctx.restore();
 }
 
 
-//draw pacman function
+//draw pacman function################################################################################################################
 function drawPacman (ctx, x, y, radius, k) {
 ctx.beginPath();
 ctx.arc(x, y, radius, (0.2*k) * Math.PI, (2-.2*k)* Math.PI);
@@ -43,53 +43,83 @@ ctx.stroke();
 }
 
 
-// draw ship
+// drawship function (very long)######################################################################################################
 
 function drawShip(ctx, radius, options) {
 options = options || {};
 let angle = (options.angle || 0.5 * Math.PI / 2);
-// this is new
-let curve = options.curve || 0.5;
+// Now we have two curve arguments
+let curve1 = options.curve1 || 0.25;
+let curve2 = options.curve2 || 0.75;
 ctx.save();
 if(options.guide) {
 ctx.strokeStyle = "white";
 ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
-
 ctx.lineWidth = 0.5;
 ctx.beginPath();
 ctx.arc(0, 0, radius, 0, 2 * Math.PI);
 ctx.stroke();
 ctx.fill();
 }
-ctx.lineWidth = options. lineWidth || 2;
+ctx.lineWidth = options.lineWidth || 2;
 ctx.strokeStyle = options.stroke || "white";
-ctx.fillStyle = options. fill || "black";
-ctx.beginPath();
-ctx.moveTo(radius, 0);
-ctx.lineTo(
+ctx.fillStyle = options.fill || "black";
+ctx.beginPath(); ctx.moveTo(radius, 0);
+// here we have the three curves
+ctx.quadraticCurveTo(
+Math.cos(angle) * radius * curve2,
+Math.sin(angle) * radius * curve2,
 Math.cos(Math.PI - angle) * radius,
 Math.sin(Math.PI - angle) * radius
 );
-// here we have added a control point based on the curve variable
-ctx.quadraticCurveTo(radius * curve - radius, 0,
+ctx.quadraticCurveTo(-radius * curve1, 0,
 Math.cos(Math.PI + angle) * radius,
+
 Math.sin(Math.PI + angle) * radius
 );
-ctx.closePath();
+ctx.quadraticCurveTo(
+Math.cos(-angle) * radius * curve2,
+Math.sin(-angle) * radius * curve2,
+radius, 0
+);
 ctx.fill();
 ctx.stroke();
-// a new guide line and circle show the control point
+// the guide drawing code is getting complicated
 if(options.guide) {
 ctx.strokeStyle = "white";
+ctx.fillStyle = "white";
 ctx.lineWidth = 0.5;
 ctx.beginPath();
+ctx.moveTo(
+Math.cos(-angle) * radius,
+Math.sin(-angle) * radius
+);
+ctx.lineTo(0, 0);
+ctx.lineTo(
+Math.cos(angle) * radius,
+Math.sin(angle) * radius
+);
 ctx.moveTo(-radius, 0);
 ctx.lineTo(0, 0);
 ctx.stroke();
 ctx.beginPath();
-ctx.arc(radius * curve - radius, 0, radius/50, 0, 2 * Math.PI);
+ctx.arc(
+Math.cos(angle) * radius * curve2,
+Math.sin(angle) * radius * curve2,
+radius/40, 0, 2 * Math.PI
+);
 
-ctx.stroke();
+ctx.fill();
+ctx.beginPath();
+ctx.arc(
+Math.cos(-angle) * radius * curve2,
+Math.sin(-angle) * radius * curve2,
+radius/40, 0, 2 * Math.PI
+);
+ctx.fill();
+ctx.beginPath();
+ctx.arc(radius * curve1 - radius, 0, radius/50, 0, 2 * Math.PI);
+ctx.fill();
 }
 ctx.restore();
 }

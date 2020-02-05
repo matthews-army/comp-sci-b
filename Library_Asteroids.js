101a2bc3c9d439da758dc212536f1797920dd1fd
matthewweir9 <43483048+matthewweir9@users.noreply.github.com>
2020-02-05 15:32:31 -0700

    Update Library_Asteroids.js

--- Library_Asteroids.js
+++ Library_Asteroids.js
@@ -127,23 +127,24 @@ ctx.restore();
 
 //Draw asteroids function (BEING CONSTRUCTED)#######################################################################################
 function drawAsteroid(ctx, radius, segments, options) {
-options = options || {};
-ctx.strokeStyle = options.stroke || "white";
-ctx.fillStyle = options.fill || "black";
-ctx.save();
-ctx.beginPath();
-for(let i = 0; i < segments; i++) {
-ctx.rotate(2 * Math.PI / segments);
-ctx.lineTo(radius + radius * options.noise * shape[i], 0);
-}
-ctx.closePath(); ctx.fill();
-ctx.stroke();
-if(options.guide) {
-ctx.lineWidth = 0.5;
+  options = options || {};
+  ctx.strokeStyle = options.stroke || "white";
+  ctx.fillStyle = options.fill || "black";
+  ctx.save();
+  ctx.beginPath();
+  for(let i = 0; i < segments; i++) {
+    ctx.rotate(2 * Math.PI / segments);
+    console.log("i: " + i);
+    ctx.lineTo(radius + radius * options.noise * (Math.random() - 0.5), 0);
+  }
+  ctx.closePath(); ctx.fill();
+  ctx.stroke();
+  if(options.guide) {
+    ctx.lineWidth = 0.5;
 
-ctx.beginPath();
-ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-ctx.stroke();
-}
-ctx.restore();
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+  ctx.restore();
 }
